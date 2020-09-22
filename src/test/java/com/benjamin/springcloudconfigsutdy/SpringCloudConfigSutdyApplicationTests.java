b77684194c122456199c9d908cc77b18e9c83b6d
fatbun <284634275@qq.com>
2020-09-22 17:00:46 +0800

    init

---
+++ src/test/java/com/benjamin/springcloudconfigsutdy/SpringCloudConfigSutdyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.benjamin.springcloudconfigsutdy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigSutdyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
